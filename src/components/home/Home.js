87e64a326c8db483b193916a631903d45e07b125
ivan limongan <ivan.limongan@gmail.com>
2018-06-14 22:31:19 -0700

    landing section added

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './home.css';
+import Landing from './userland-landing.jpg';
+
 
 class Home extends Component {
 
   render() { 
     return (
       <section className="main-container maxwidth-wrap">
-    
+
+        <div className="landing-container">
+          <h1 className="landing-title">Welcome to UserLAnd Technologies</h1>
+          <img className="landing-img" src={Landing} alt="Userland landing"/>
+        </div>
 
       </section>
     );
