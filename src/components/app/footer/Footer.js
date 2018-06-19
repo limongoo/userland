efc7cc5cefa2169d29dc8ae65478cb46cd0c7863
ivan limongan <ivan.limongan@gmail.com>
2018-06-18 22:59:10 -0700

    minor style

--- src/components/app/footer/Footer.js
+++ src/components/app/footer/Footer.js
@@ -10,14 +10,16 @@ export default class Footer extends Component {
           
           {/* --- Contact Section --- */}
           <div className="contact-container">
-            <div className="maxwidth-wrap">
+            <div className="maxwidth-wrap container">
               {/* Title goes here */}
-              <h3 className="contact-title">Contact</h3>
+              <h4 className="contact-title">Contact</h4>
           
               {/* Text goes here */}
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              <p>&#169; <a href="https://github.com/limongoo/" target="_blank" rel="author noopener noreferrer">UserLAnd Technologies, LLC.</a></p>
+              <p>&#169; UserLAnd Technologies, LLC.</p>
+              
             </div>
+            {/* <div className="layer-footer"></div> */}
           </div>
           
         </section>
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -26,11 +26,15 @@ class Home extends Component {
         {/* --- About Us Section --- */}
         <div className="about-container">
           <div className="maxwidth-wrap">
-            {/* Title goes here */}
-            <h4 className="about-title"> About UserLAnd Technologies</h4>
+            
+            <div className="about-content">
+              {/* Title goes here */}
+              <h4 className="about-title"> About UserLAnd Technologies</h4>
+            
+              {/* Text goes here */}
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           
-            {/* Text goes here */}
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
           </div>
         </div>
 
