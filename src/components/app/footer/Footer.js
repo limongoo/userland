6676e6478b6dd6efd9b0e2743034b1dc6ca56c16
ivan limongan <ivan.limongan@gmail.com>
2018-06-20 22:42:50 -0700

    content update

--- src/components/app/footer/Footer.js
+++ src/components/app/footer/Footer.js
@@ -15,9 +15,10 @@ export default class Footer extends Component {
               <h4 className="contact-title">Contact</h4>
           
               {/* Text goes here */}
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              <p>&#169; UserLAnd Technologies, LLC.</p>
-              
+              <div className="contact-flex">
+                <p></p>
+                <p>&#169; UserLAnd Technologies, LLC.</p>
+              </div>
             </div>
             {/* <div className="layer-footer"></div> */}
           </div>
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './home.css';
-import Landing from './userland-landing.jpg';
 
 
 class Home extends Component {
@@ -27,10 +26,14 @@ class Home extends Component {
         <div className="about-container">
           <div className="maxwidth-wrap">
             
-            <div className="about-content">
-              {/* Title goes here */}
-              <h4 className="about-title"> About UserLAnd Technologies</h4>
+            {/* Title goes here */}
+            <h5 className="about-title"> About UserLAnd Technologies</h5>
+            <div className="title-block"></div>
             
+            <div className="about-content">
+
+              <h3>We’re a company founded on the belief that good ideas deserve to be made.</h3>
+
               {/* Text goes here */}
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           
@@ -43,11 +46,16 @@ class Home extends Component {
         {/* --- Product Section --- */}
         <div className="product-container">
           <div className="maxwidth-wrap">
-            {/* Title goes here */}
-            <h4 className="product-title">Product</h4>
-          
-            {/* Text goes here */}
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            
+            <div className="product-wrap">
+              {/* Title goes here */}
+              <h4 className="product-title">Power for what you do today. 
+              <br/>And what you’ll discover tomorrow.</h4>
+              <div className="title-block-product"></div>
+
+              {/* Text goes here */}
+              <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            </div>
           </div>
         </div>
 
@@ -57,10 +65,51 @@ class Home extends Component {
           <div className="maxwidth-wrap">
           
             {/* Title goes here */}
-            <h4 className="job-title">Jobs</h4>
+            <h5 className="about-title"> Jobs at UserLAnd</h5>
+            <div className="title-block"></div>
           
             {/* Text goes here */}
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div className="job-section">
+              <ul className="jobs-ul">
+
+                <li className="jobs-li">
+                  <div className="jobs-title">
+                    Back-End Developer
+                  </div>
+                  <div className="jobs-description">
+                    <p>A back-end web developer is responsible for server-side web application logic and integration of the work front-end developers do. Back-end developers are usually write the web services and APIs used by front-end developers and mobile application developers.</p>
+                    <ul className="jobs-sub-ul">
+                      <li>You are smart and keep your head up in the face of a challenge.</li>
+                      <li>You are a quick study and enjoy learning new things.</li>
+                      <li>You have a demonstrated aptitude for software development.</li>
+                      <li>You are not afraid to dive into complex codebases teeming with both the new and shiny as well as the ancient and mysterious.</li>
+                      <li>You diagnose and squash technical debt with glee.</li>
+                      <li>You like helping others, regardless of their technical prowess.</li>
+                    </ul>
+                    <p>To apply, email your resume and cover letter to <strong><a href="mailto:jobs@userlandtech.com">jobs@userlandtech.com</a></strong></p>
+                  </div>
+                </li>
+
+                <li className="jobs-li">
+                  <div className="jobs-title">
+                    Back-End Developer
+                  </div>
+                  <div className="jobs-description">
+                    <p>A back-end web developer is responsible for server-side web application logic and integration of the work front-end developers do. Back-end developers are usually write the web services and APIs used by front-end developers and mobile application developers.</p>
+                    <ul className="jobs-sub-ul">
+                      <li>You are smart and keep your head up in the face of a challenge.</li>
+                      <li>You are a quick study and enjoy learning new things.</li>
+                      <li>You have a demonstrated aptitude for software development.</li>
+                      <li>You are not afraid to dive into complex codebases teeming with both the new and shiny as well as the ancient and mysterious.</li>
+                      <li>You diagnose and squash technical debt with glee.</li>
+                      <li>You like helping others, regardless of their technical prowess.</li>
+                    </ul>
+                    <p>To apply, email your resume and cover letter to <strong><a href="mailto:jobs@userlandtech.com">jobs@userlandtech.com</a></strong></p>
+                  </div>
+                </li>
+                
+              </ul>
+            </div>
           </div>
         </div>
 
