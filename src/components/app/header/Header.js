37edf32d30919ad78a51074079971b9283cc8962
ivan limongan <ivan.limongan@gmail.com>
2018-07-13 15:16:08 -0700

    headroom style

--- src/components/app/header/Header.js
+++ src/components/app/header/Header.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import '../app.css';
 import Error from '../errorloading/Error';
 import Logo from './UserLand-Logo-Hex.png';
+import Headroom from 'react-headroom';
 
 
 class Header extends Component {
@@ -33,36 +34,41 @@ class Header extends Component {
     const { menu } = this.state;
 
     return (
-      <header role="banner" id="header">
-        <section className="head-container">
+      <Headroom>
+        <header role="banner" id="header">
+          <section className="head-container">
           
-          <div className="maxwidth-wrap nav-wrap">
+            <div className="maxwidth-wrap nav-wrap">
 
-            {/* Navigation Logo */}
-            <Link to="/" className="no-line"><img className="logo" src={Logo} alt="UserLAnd Logo"/></Link>
+              {/* Navigation Logo */}
+              <Link to="/" className="no-line"><img className="logo" src={Logo} alt="UserLAnd Logo"/></Link>
           
-            <div>
-              <label htmlFor="check" name="checkbox"></label>
-              <input id="check" className="checkbox" type="checkbox" onClick={this.handleClick} checked={menu}/>
+              <div>
+                <label htmlFor="check" name="checkbox"></label>
+                <input id="check" className="checkbox" type="checkbox" onClick={this.handleClick} checked={menu}/>
 
-              <div id="burger"><span></span><span></span><span></span></div>
-              <nav id="main-menu">
+                <div id="burger"><span></span><span></span><span></span></div>
+                <nav id="main-menu">
 
-                {/* Mobile Menu Logo */}
-                <Link to="/" className="no-line"><img className="mobile-logo" src={Logo} alt="UserLAnd Logo"/></Link>
+                  {/* Mobile Menu Logo */}
+                  <Link to="/" className="no-line"><img className="mobile-logo" src={Logo} alt="UserLAnd Logo"/></Link>
                 
-                {/* Navigation Menu */}
-                <ul className="nav-ul">
-                  <li><Link to="/product" href="#product" onClick={this.handleClick}>Product</Link></li>
-                  <li><Link to="/about" onClick={this.handleClick}>About Us</Link></li>
-                  <li><Link to="/jobs" href="#jobs" onClick={this.handleClick}>Jobs</Link></li>
-                </ul>
-              </nav>
+                  {/* Navigation Menu */}
+                  <ul className="nav-ul">
+                    <li><Link to="/product" href="#product" onClick={this.handleClick}>Product</Link></li>
+                    <li><Link to="/about" onClick={this.handleClick}>About Us</Link></li>
+                    <li><Link to="/jobs" href="#jobs" onClick={this.handleClick}>Jobs</Link></li>
+                  </ul>
+                </nav>
+              </div>
             </div>
-          </div>
-          {error && <Error error={error}/>}
-        </section> 
-      </header>
+            {error && <Error error={error}/>}
+
+         
+          
+          </section> 
+        </header>
+      </Headroom>
     );
   }
 }
