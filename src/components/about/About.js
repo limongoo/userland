de95a8ed133e0f45ae8053b4e53ce203a22b6665
ivan limongan <ivan.limongan@gmail.com>
2018-07-16 17:20:05 -0700

    animation delay added

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -62,25 +62,25 @@ class About extends Component {
             <div className="core-content">
               
               {/* Text goes here */}
-              <div>
+              <ScrollAnimation animateIn='fadeIn' delay='100'>
                 <img className="core-img" src={CoreImg} alt="Secure"/>
                 <h4>Secure</h4>
                 <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              </div>
+              </ScrollAnimation>
 
               {/* Text goes here */}
-              <div>
+              <ScrollAnimation animateIn='fadeIn' delay='400'>
                 <img className="core-img" src={CoreImg} alt="Secure"/>
                 <h4>Safe</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              </div>
+              </ScrollAnimation>
 
               {/* Text goes here */}
-              <div>
+              <ScrollAnimation animateIn='fadeIn' delay='800'>
                 <img className="core-img" src={CoreImg} alt="Secure"/>
                 <h4>Accessible</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-              </div>
+              </ScrollAnimation>
 
             </div>
           </div>
