e23d0adb6ebdcb8ddbac663196cff55922b5f361
ivan limongan <ivan.limongan@gmail.com>
2018-07-18 22:41:05 -0700

    pulse button added

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -24,7 +24,7 @@ class Home extends Component {
               {/* <h1 className="landing-title">The Possibilities Are Endless</h1> */}
               <div className="landing-img"><img src={LandingImg} alt="Userland logo"/></div>
               <div className="pulse-wrap">
-                <button className="pulse-button"></button>
+                <a href="#product" className="pulse-link"><button className="pulse-button"></button></a>
               </div>
             </div>
           </div>
